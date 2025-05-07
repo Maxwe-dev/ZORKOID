4782055ce93ed3af5467eb5bf2f0ffb249774921
Maxwe-dev <katkreator10010111@gmail.com>
2025-05-06 19:36:46 -0700

    Create Updater

---
+++ Update all Games.lua
@@ -0,0 +1,2 @@
+os.execute("wget -f https://raw.githubusercontent.com/Maxwe-dev/ZORKOID/refs/heads/main/ZORKOID.lua ZORKOID.lua")
+os.execute("wget -f https://raw.githubusercontent.com/Maxwe-dev/ZORKOID/refs/heads/main/XORKOID.lua XORKOID.lua")
